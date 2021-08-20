4514df51d37cdd84c8189fc28b84dc711be1036b
AB075440 <Abhishek.Budruk@Cerner.com>
2021-08-20 23:56:03 +0530

    added payment to admin

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Product , Wishlist ,OrderItem , Order , CheckoutAddress
+from .models import Product , Wishlist ,OrderItem , Order , CheckoutAddress , Payment
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Wishlist)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(CheckoutAddress)
+admin.site.register(Payment)
 
 
